432a365861f72fd7c32785a49cc4a81422b043f9
yayanberutu <yosepri.disyandro@dana.id>
2024-07-23 21:06:19 +0700

    initiate project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author Yosepri Disyandro Berutu (yosepri.disyandro@dana.id)
+ * @version $Id: ${NAME}.java , v 0.1 2024-07-22 13.31 Yosepri Disyandro Berutu Exp $$
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
